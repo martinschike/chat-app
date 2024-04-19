5f536c15491b694714cb46d0da2fab3175bf03dc
Martins Okafor <okaformartinschinwike@gmail.com>
2024-04-19 21:25:28 +0100

    completed search, messages section

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import useConversation from "../../store/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
@@ -5,6 +6,11 @@ import { TiMessage } from "react-icons/ti";
 
 const MessageContainer = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    // cleanup function (unmounts)
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
 
   return (
     <div className="md:min-w-[450px] flex flex-col">
@@ -15,7 +21,9 @@ const MessageContainer = () => {
           {/* Header */}
           <div className="bg-slate-500 px-4 py-2 mb-2">
             <span className="label-text">To:</span>{" "}
-            <span className="text-gray-900 font-bold">John Doe</span>
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.fullName}
+            </span>
           </div>
 
           <Messages />
